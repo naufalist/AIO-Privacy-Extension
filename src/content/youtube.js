e8eee8ad1e8fb0598f7bab7db92401d3bc18e014
Naufal <contact.naufalist@gmail.com>
2025-07-28 11:42:12 +0700

    fix(youtube): wrong css file name

--- src/content/youtube.js
+++ src/content/youtube.js
@@ -1,4 +1,4 @@
 (async () => {
   const { initSite } = await import(chrome.runtime.getURL("../core/initSite.js"));
-  initSite("youtube", "css/facebook.css");
+  initSite("youtube", "css/youtube.css");
 })();
