d5215ef19936e8194df5e9e3984c81d1c72310c6
Naufal <contact.naufalist@gmail.com>
2026-04-08 10:22:36 +0700

    refactor(content): remove cssPath param from initSite() calls
    CSS injection is now handled internally by initSite via cssGenerator,
    so the cssPath argument is no longer needed.

--- src/content/shopee.js
+++ src/content/shopee.js
@@ -1,4 +1,4 @@
 (async () => {
   const { initSite } = await import(chrome.runtime.getURL("../core/initSite.js"));
-  initSite("shopee", "css/shopee.css");
+  initSite("shopee");
 })();
--- src/content/twitter.js
+++ src/content/twitter.js
@@ -1,4 +1,4 @@
 (async () => {
   const { initSite } = await import(chrome.runtime.getURL("../core/initSite.js"));
-  initSite("twitter", "css/twitter.css");
+  initSite("twitter");
 })();
